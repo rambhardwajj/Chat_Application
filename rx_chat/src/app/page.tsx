d534e5b748792ee30542d93d32d8c86b6c2dce84
rambhardwajj <rambhardwaj101000@gmail.com>
2023-12-08 19:08:18 +0530

    Database Integration Done through Redis

--- rx_chat/src/app/page.tsx
+++ rx_chat/src/app/page.tsx
@@ -1,7 +1,9 @@
+import { db } from '@/lib/db'
 import Image from 'next/image'
 
-export default function Home() {
+export default  async function Home() {
+  await db.set( "hello2", "hello2")
   return (
     <div className='text-red-500'> hello </div>
-      )
+    )
 }
